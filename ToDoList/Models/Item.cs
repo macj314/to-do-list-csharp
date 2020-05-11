5566d695078cee1bcd873c83753163753bcd83e6
unknown <macjay314@gmail.com>
2020-05-11 14:25:05 -0700

    Create a static List to hold all Item objects. Create a method to retrieve that List

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,11 +2,18 @@ namespace ToDoList.Models
 {
   public class Item
   {
-    public string Description { get; }
+    public string Description { get; set; }
+    private static ToDoList<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
+      _instances.Add(this);
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
     }
   }
 }
