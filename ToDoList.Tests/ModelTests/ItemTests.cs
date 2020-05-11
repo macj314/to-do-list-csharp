155f43fd41d14d5055f660020f6dd2d2b35aa0f6
unknown <macjay314@gmail.com>
2020-05-11 13:53:56 -0700

    Create test that checks the instantiation and call of Item object with Description property

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -7,7 +7,26 @@ namespace ToDoList.Tests
   public class ItemTests
   {
 
-    // Test methods will go here.
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act      
+      string result = newItem.description;
+      
+      //Assert
+      Assert.AreEqual(description, result);
+    }
 
   }
 }
