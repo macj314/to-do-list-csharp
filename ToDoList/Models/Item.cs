b6842b096118cee9681c6ff787d6a3fe8af50ace
unknown <macjay314@gmail.com>
2020-05-20 10:23:43 -0700

    Integrate MVC Framework

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,21 +1,16 @@
 using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
     public string Description { get; set; }
-    public int Priority { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
-        Description = description;
-        _instances.Add(this);
-    }
-    public Item(string description, int priority)
-      : this(description)
-    {
-      Priority = priority;
+      Description = description;
+      _instances.Add(this);
     }
 
     public static List<Item> GetAll()
@@ -27,5 +22,6 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
+
   }
 }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,37 +1,20 @@
-using System;
-using ToDoList.Models;
-using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList{
-  public class Program{
-  
-    public static void StartGame(){
-      Console.WriteLine("Welcome to the To Do List");
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View/back)");
-      string userInput = Console.ReadLine();
-      userInput.ToLower();
-      if(userInput == "view"){
-        List<Item> newList = Item.GetAll();
-        if(newList.Count > 0){
-          foreach(Item item in newList){
-          Console.WriteLine(item.Description);
-          }
-          StartGame();
-        }else{
-          Console.WriteLine("Todolist is empty list");
-        }
-        
-      }else if(userInput == "add"){
-        Console.WriteLine("Please enter the description for the new item.");
-        string description = Console.ReadLine();
-        Item newItem = new Item(description);
-        StartGame();
-      }else{
-        Console.WriteLine("Good Bye");
-      }
+namespace ToDoList
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
-    public static void Main(){
-      StartGame();
-    } 
   }
 }
