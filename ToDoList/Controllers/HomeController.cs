6e3bd5c2dc25fc567f6944fe6348e36f219447e7
unknown <macjay314@gmail.com>
2020-05-27 15:51:52 -0700

    Integrate EF

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -2,20 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ToDoList.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
-      [Route("/favorite_photos")]
-      public ActionResult FavoritePhotos()
-      {
-        return View();
-      }
-
+      return View();
     }
+  }
 }
